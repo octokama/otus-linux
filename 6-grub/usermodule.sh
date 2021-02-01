3d99af2cbb640df077220f3b497f9a096d2fe710
octokama <gpbu4407@int.gazprombank.ru>
2021-02-01 21:02:26 +0300

    (+) add 6

---
+++ 6-grub/usermodule.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cat <<'msgend'
+Hello! You are in dracut module!
+ ___________________
+< I'm dracut module >
+ -------------------
+   \
+    \
+        .--.
+       |o_o |
+       |:_/ |
+      //   \ \
+     (|     | )
+    /'\_   _/`\
+    \___)=(___/
+msgend
+sleep 10
+echo " continuing...."
